7112b82bf520a6262a905f9e624a88f22f464399
alexkhomov <alexkhomov2012@gmail.com>
2020-01-12 12:32:28 -0500

    what the ha

--- draw.py
+++ draw.py
@@ -1,7 +1,11 @@
 import turtle
 
 painter = turtle.Turtle()
+<<<<<<< HEAD
+painter.pensize(200)
+=======
 painter.pensize(10)
+>>>>>>> parent of dd732e7... longer line
 
 for _ in range(12):
     painter.forward(300)
